728b452d94dd53ff36842f69af6bb3a68bfda9ca
Rob Yoder <rob@agilebits.com>
2019-07-24 17:56:34 -0600

    Fix tests

--- tests/test-browser.js
+++ tests/test-browser.js
@@ -1,64 +1,34 @@
 "use strict";
 
-let test = require("tape");
-let emergencyKit = require("../lib/emergency-kit-node.js")
+import test from "tape";
 
 function setup() {
-    let k = window.emergencyKit({
+    return emergencyKit.create({
         email: "wendyappleseed@me.com",
         name: "Wendy Appleseed",
         accountKey: "AK-123-456",
         domain: "appleseed",
         teamURL: "appleseed.1password.com",
-        //qrCode: image-data
     });
-    return k;
 }
-
 
 function runBlobTests(blob) {
-    let content = new Uint8Array(1000);
-    let referenceBlob = new Blob(content, {type:"application/pdf"});
-
-    test("Streamed to a blob.", (assert) => {
-            let expected = referenceBlob instanceof Blob;
-            let actual = blob instanceof Blob;
-            assert.equal(expected, actual, blob);
-            assert.end();
-    });
-
-    test("Blob has content in it.", (assert) => {
-            let expected = referenceBlob.size > 0;
-            let actual = blob.size > 0;
-            assert.equal(expected, actual, blob.size);
-            assert.end();
-    });
-
-    test("Blob is of type 'application/pdf'.", (assert) => {
-            assert.equal(blob.type, referenceBlob.type, blob.type);
-            assert.end();
+    test("Blob", (assert) => {
+        assert.equal(blob instanceof Blob, true, "should be a Blob");
+        assert.equal(blob.size > 0, true, `should have content: ${blob.size}`);
+        assert.equal(blob.type, "application/pdf", "should have correct type");
+        assert.end();
     });
 }
 
-function runURLTests(url) {
-    let content = new Uint8Array(1000);
-    let referenceBlob = new Blob(content, {type:"application/pdf"});
-    let referenceURL = URL.createObjectURL(referenceBlob);
-
-    test("Streamed to a string.", (assert) => {
-            let expected = typeof referenceURL == "string";
-            let actual = typeof url == "string";
-            assert.equal(expected, actual, url);
-            assert.end();
-    });
-
-    test("String is a blob URL.", (assert) => {
-            let expected = referenceURL.startsWith('blob:');
-            let actual = url.startsWith('blob:');
-            assert.equal(expected, actual);
-            assert.end();
+function runUrlTests(url) {
+    test("URL", (assert) => {
+        assert.equal(typeof url, "string", "should be a string");
+        assert.equal(url.startsWith('blob:'), true, "should be a Blob URL");
+        assert.end();
     });
 }
 
 setup().toBlob().then(runBlobTests);
-setup().toURL().then(runURLTests);
+setup().toURL().then(runUrlTests);
+test.onFinish(() => window.close());
